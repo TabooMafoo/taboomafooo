ae1d0065dba940c38dfcaf5703167d4464188447
Mirkan <mirkancaliskan@gmail.com>
2018-11-15 19:50:29 +0300

    Updates

---
+++ precache-manifest.773df35b4ec50aac2e9d1224703bdc9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "909fbf710d33bb625561",
+    "url": "/static/js/main.909fbf71.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "909fbf710d33bb625561",
+    "url": "/static/css/main.276a7f6d.chunk.css"
+  },
+  {
+    "revision": "8f36a6be7fdd8e88d4d8a946160876cc",
+    "url": "/index.html"
+  }
+];
